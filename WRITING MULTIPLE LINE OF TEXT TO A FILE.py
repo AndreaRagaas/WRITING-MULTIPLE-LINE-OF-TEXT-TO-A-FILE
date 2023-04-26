821f1a82d6a0b9368d0926fd1b49db0259cf9bd5
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 11:40:22 +0800

    ask the user to enter the line of text

--- WRITING MULTIPLE LINE OF TEXT TO A FILE.py
+++ WRITING MULTIPLE LINE OF TEXT TO A FILE.py
@@ -1,2 +1,10 @@
 #creating a file to write the text input by the user
 my_file = open("mylife.txt","w")
+
+#ask the user to enter the line of text
+while True:
+        line = input('Enter line: ')
+        if line:
+            my_file.write(line + '\n')
+        else:
+              break
