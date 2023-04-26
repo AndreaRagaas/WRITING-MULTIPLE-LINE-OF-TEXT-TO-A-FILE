0bdea0abf38beb5a6a123bc98c3afa187a3178ef
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 11:46:54 +0800

    WRITING MULTIPLE LINE OF TEXT TO A FILE

--- WRITING MULTIPLE LINE OF TEXT TO A FILE.py
+++ WRITING MULTIPLE LINE OF TEXT TO A FILE.py
@@ -1,3 +1,8 @@
+#2022-09805-MN-0
+#RAGAAS, ANDREA 
+#BSCPE 1-5
+#A method in python to write multiple line of text contents into a text file mylife.txt.
+
 #creating a file to write the text input by the user
 my_file = open("mylife.txt","w")
 
